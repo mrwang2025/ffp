c46304a9a77d88f11a5f09d18c21dd2a0288c619
mrwang2025 <mrwang2025@gmail.com>
2021-11-16 00:25:05 -0800

    upgrade to v1.0.8
    Expose the filters class definitions

--- pack/src/index.js
+++ pack/src/index.js
@@ -1,7 +1,15 @@
 import { EFieldType } from "./fieldFilterControl";
 import FieldFilterControlGroup from './fieldFilterControlGroup';
+import { FieldDateRangeFilter, FieldNameFilter, FieldValueRangeFilter, Filter, FilterGroup, FilterGroupAndGate, FilterGroupOrGate } from './filter';
 
 export {
     EFieldType,
-    FieldFilterControlGroup
+    FieldFilterControlGroup,
+    Filter,
+    FieldNameFilter,
+    FieldValueRangeFilter,
+    FieldDateRangeFilter,
+    FilterGroup,
+    FilterGroupOrGate,
+    FilterGroupAndGate
 };
